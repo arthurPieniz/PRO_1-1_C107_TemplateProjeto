b60d6e90f692bf1783c33bc3fd477416d83d3d35
arthurPieniz <artur.pieniz@gmail.com>
2023-06-16 11:06:10 -0300

    'resolucao'

--- obj_trk_final.py
+++ obj_trk_final.py
@@ -41,19 +41,37 @@ def goal_track(img,bbox):
         cv2.circle(img,(xs[i],ys[i]),2,(0,0,255),5)
 
 def drawBox(img,bbox):
-    x,y,w,h = int(bbox[0]),int(bbox[1]),int(bbox[2]),int(bbox[3])
+    #escreva o código desta função aqui
+    x, y, w, h = int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3])
+
     cv2.rectangle(img,(x,y),((x+w),(y+h)),(255,0,255),3,1)
+
     cv2.putText(img,"Rastreando",(75,90),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,255,0),2)
 
 
-
-def drawBox(img,bbox):
-    #escreva o código desta função aqui
-    pass
-
-
 while True:
-   #escreva o código dentro do loop aqui
-   pass
+    check, img = video.read()   
+
+    # Atualize o rastreador em img e na caixa delimitadora
+    success, bbox = tracker.update(img)
+
+    if success:
+        drawBox(img, bbox)
+    else:
+        cv2.putText(img,"Errou",(75,90),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
+
+    # Chame o goal_track()
+    goal_track(img, bbox)
+
+    # Exiba o vídeo
+    cv2.imshow("resultado", img)
+            
+
+    # Saia da janele quando pressionar a tecla espaço        
+    key = cv2.waitKey(25)
+    if key == 32:
+        print("Interrompido")
+        break
+
 video.release()
 cv2.destroyALLwindows() 
